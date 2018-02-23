a2b8ec06cebd9e6e396c68e491dd61bea0ef8224
Héctor García García <Hectoruno>
2018-02-23 09:00:27 +0100

    Creamos unas bolas de distinto radio y color, las cuales deben ir botando hasta el final de la pantalla, cuando lleguen a ese punto se acaba la animación

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,6 +12,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private BouncingBall[] myBalls;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -23,14 +25,33 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
         int ground = 400;   // position of the ground line
+        
+        myBalls = new BouncingBall[numberOfBalls];
 
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
+        
+        Random aleatorio = new Random();
+        
+        for (int i = 0; i < numberOfBalls; i++) {
+            int radio = aleatorio.nextInt(20) + 5;
+            int red = aleatorio.nextInt(256);
+            int green = aleatorio.nextInt(256);
+            int blue = aleatorio.nextInt(256);
+            
+            Color color = new Color(red, green, blue);
+            
+            int posX = aleatorio.nextInt(300);
+            
+            BouncingBall ball = new BouncingBall(posX, 50, radio, color, ground, myCanvas);
+            ball.draw();
+            myBalls[i] = ball;
+        }
 
         // crate and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
@@ -42,11 +63,15 @@ public class BallDemo
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            
+            for (int i = 0; i < numberOfBalls; i++) {
+                myBalls[i].move();
+                
+                // stop once ball has travelled a certain distance on x axis
+                if(myBalls[i].getXPosition() >= 550){ 
+                    finished = true;
+                }               
+            
             }
         }
     }
